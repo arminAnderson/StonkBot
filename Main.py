298faadcacbcce9f83db74b3044960a538e9dff2
Armin <arminkanderson@hotmail.com>
2020-08-17 23:42:01 -0700

    Getting history and opening robinhood:

--- Main.py
+++ Main.py
@@ -1,15 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
 
 def GetPrice(name):
     URL = "https://finance.yahoo.com/quote/" + name
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
-    result = soup.find(class_="C($primaryColor) Fz(24px) Fw(b)")
+    result = soup.find(class_="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)")
     print(result.text)
 
+def GetPerformance(name):
+    URL = "https://finance.yahoo.com/quote/" + name + "/history?p=MFA"
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    result = soup.find("tbody")
+    table = result.findAll("tr", limit=1000)
+
+    history = []
+    dividend = 0
+    print("Table size: " + str(len(table)))
+    for row in table:
+        cell = row.findAll("td")
+        try:
+            history.append(float(cell[4].text))
+        except(IndexError):
+            dividend += float(cell[1].text.split()[0])
+
+    print(history)
+    print("Dividends: " + "{0:g}".format(dividend))
+
+commands = set()
+commands.add("price")
+
+url='https://robinhood.com/stocks/KNDI'
+webbrowser.open(url)
+
+GetPerformance("MFA")
+
 while(True):
-    name = input()
-    if name == "exit":
+    command = ""
+    arg = ""
+
+    if command == "exit":
         break
-    GetPrice(name)
+
+    try:
+        command, arg = map(str, input().split())
+    except (ValueError):
+        print("Invalid command")
+        continue
+
+    if command in commands: 
+        if command == "price":
+            GetPrice(arg)
+    else:
+        print("Invalid command")
